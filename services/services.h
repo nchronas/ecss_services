fb3a72e82f2daf8e5d322ac9879c95f2002a9703
nchronas <nchronas@gmail.com>
2018-01-16 22:40:37 +0100

    Removed application specific code

--- services/services.h
+++ services/services.h
@@ -1,18 +1,9 @@
 #ifndef __SERVICES_H
 #define __SERVICES_H
 
-#include <stdio.h>
-#include <stdint.h>
-#include <stdbool.h>
-#include <string.h>
-#include "subsystems_ids.h"
-#include "system.h"
-
 /* TM TC services*/
 #define ECSS_VER_NUMBER             0
 #define ECSS_DATA_FIELD_HDR_FLG     1
-// never used
-//#define TC_TM_SER_TC_VER            1
 
 #define ECSS_PUS_VER            1
 #define ECSS_SEC_HDR_FIELD_FLG  0
@@ -39,107 +30,8 @@
 
 #define ECSS_DATA_OFFSET        10  /*ECSS_HEADER_SIZE + ECSS_DATA_HEADER_SIZE*/
 
-#define MAX_APP_ID      20
-#define MAX_SERVICES    20
-#define MAX_SUBTYPES    26
-
 #define TC 1
 #define TM 0
-
-//needs to redifine
-#define MAX_PKT_LEN         210 /*ECSS_HEADER_SIZE + ECSS_DATA_HEADER_SIZE + MAX_PKT_DATA + ECSS_CRC_SIZE*/
-
-#define MAX_PKT_DATA        198
-#define TC_MAX_PKT_SIZE     210
-#define TC_MIN_PKT_SIZE     11 //12  /*ECSS_HEADER_SIZE + ECSS_DATA_HEADER_SIZE + ECSS_CRC_SIZE*/
-
-#define MAX_PKT_EXT_DATA    2051
-#define TC_MAX_PKT_EXT_SIZE 2063
-
-#if (SYSTEM_APP_ID == _EPS_APP_ID_)
-#define MAX_PKT_SIZE  210
-#else
-#define MAX_PKT_SIZE  2063
-#endif
-
-#define MS_SU_LOG_SIZE  196
-#define MS_WOD_LOG_SIZE  228
-
-typedef enum {
-    OBC_APP_ID      = _OBC_APP_ID_,
-    EPS_APP_ID      = _EPS_APP_ID_,
-    ADCS_APP_ID     = _ADCS_APP_ID_,
-    COMMS_APP_ID    = _COMMS_APP_ID_,
-    IAC_APP_ID      = _IAC_APP_ID_,
-    GND_APP_ID      = _GND_APP_ID_,
-    DBG_APP_ID      = _DBG_APP_ID_,
-    LAST_APP_ID     = _LAST_APP_ID_
-}TC_TM_app_id;
-
-typedef enum {
-    SATR_PKT_ILLEGAL_APPID     = 0,
-    SATR_PKT_INV_LEN           = 1,
-    SATR_PKT_INC_CRC           = 2,
-    SATR_PKT_ILLEGAL_PKT_TP    = 3,
-    SATR_PKT_ILLEGAL_PKT_STP   = 4,
-    SATR_PKT_ILLEGAL_APP_DATA  = 5,
-    SATR_OK                    = 6,
-    SATR_ERROR                 = 7,
-    SATR_EOT                   = 8,
-    SATR_CRC_ERROR             = 9,
-    SATR_PKT_ILLEGAL_ACK       = 10,
-    SATR_ALREADY_SERVICING     = 11,
-    SATR_MS_MAX_FILES          = 12,
-    SATR_PKT_INIT              = 13,
-    SATR_INV_STORE_ID          = 14,
-    SATR_INV_DATA_LEN          = 15,
-    /* Scheduling Service Error State Codes
-    * from
-    */
-    SATR_SCHS_FULL             = 16, /*Schedule array is full */
-    SATR_SCHS_ID_INVALID       = 17, /*Sub-schedule ID invalid */
-    SATR_SCHS_NMR_OF_TC_INVLD  = 18, /*Number of telecommands invalid */
-    SATR_SCHS_INTRL_ID_INVLD   = 19, /*Interlock ID invalid */
-    SATR_SCHS_ASS_INTRL_ID_INVLD = 20, /*Assess Interlock ID invalid */
-    SATR_SCHS_ASS_TP_ID_INVLD  = 21, /*Assessment type id invalid*/
-    SATR_SCHS_RLS_TT_ID_INVLD  = 22, /*Release time type ID invalid */
-    SATR_SCHS_DST_APID_INVLD   = 23, /*Destination APID in embedded TC is invalid */
-    SATR_SCHS_TIM_INVLD        = 24, /*Release time of TC is invalid */
-    SATR_QBTIME_INVALID        = 25, /*Time management reported erroneous time*/
-    SATR_SCHS_TIM_SPEC_INVLD   = 26, /*Release time of TC is specified in a invalid representation*/
-    SATR_SCHS_INTRL_LGC_ERR    = 27, /*The release time of telecommand is in the execution window of its interlocking telecommand.*/
-    SATR_SCHS_DISABLED         = 28,
-    SATR_SCHS_NOT_IMLP         = 29, /*Not implemented function of scheduling service*/
-    /*FatFs*/
-    SATRF_OK                   = 30, /* (0) Succeeded */
-    SATRF_DISK_ERR             = 31, /* (1) A hard error occurred in the low level disk I/O layer */
-    SATRF_INT_ERR              = 32, /* (2) Assertion failed */
-    SATRF_NOT_READY            = 33, /* (3) The physical drive cannot work */
-    SATRF_NO_FILE              = 34, /* (4) Could not find the file */
-    SATRF_NO_PATH              = 35, /* (5) Could not find the path */
-    SATRF_INVALID_NAME         = 36, /* (6) The path name format is invalid */
-    SATRF_DENIED               = 37, /* (7) Access denied due to prohibited access or directory full */
-    SATRF_EXIST                = 38, /* (8) Access denied due to prohibited access */
-    SATRF_INVALID_OBJECT       = 39, /* (9) The file/directory object is invalid */
-    SATRF_WRITE_PROTECTED      = 40, /* (10) The physical drive is write protected */
-    SATRF_INVALID_DRIVE        = 41, /* (11) The logical drive number is invalid */
-    SATRF_NOT_ENABLED          = 42, /* (12) The volume has no work area */
-    SATRF_NO_FILESYSTEM        = 43, /* (13) There is no valid FAT volume */
-    SATRF_MKFS_ABORTED         = 44, /* (14) The f_mkfs() aborted due to any parameter error */
-    SATRF_TIMEOUT              = 45, /* (15) Could not get a grant to access the volume within defined period */
-    SATRF_LOCKED               = 46, /* (16) The operation is rejected according to the file sharing policy */
-    SATRF_NOT_ENOUGH_CORE      = 47, /* (17) LFN working buffer could not be allocated */
-    SATRF_TOO_MANY_OPEN_FILES  = 48, /* (18) Number of open files > _FS_SHARE */
-    SATRF_INVALID_PARAMETER    = 49, /* (19) Given parameter is invalid */
-    SATRF_DIR_ERROR            = 50,
-    SATR_SD_DISABLED           = 51,
-    SATR_QUEUE_FULL            = 52,
-    SATR_WRONG_DOWNLINK_OFFSET = 53,
-    SATR_VER_ERROR             = 54,
-	SATR_FIREWALLED            = 55,
-    /*LAST*/
-    SATR_LAST                  = 56
-}SAT_returnState;
 
 /*services types*/
 #define TC_VERIFICATION_SERVICE         1
@@ -160,8 +52,8 @@ typedef enum {
 #define TC_HK_REPORT_PARAMETERS         21
 #define TM_HK_PARAMETERS_REPORT         23
 
-#define TM_EV_NORMAL_REPORT         	1
-#define TM_EV_ERROR_REPORT         	4
+#define TM_EV_NORMAL_REPORT         	  1
+#define TM_EV_ERROR_REPORT         	    4
 
 #define TC_FM_PERFORM_FUNCTION          1
 
@@ -250,162 +142,5 @@ typedef enum {
 #define SCHS_REPORT_SCH_SUMMARY         17 /*subservice 17, Telecommand to report schedules in summary form*/
 #define SCHS_LOAD_SCHEDULES             22 /*subservice 22, Telecommand to load schedules from perm storage*/
 #define SCHS_SAVE_SCHEDULES             23 /*subservice 23, Telecommand to save schedules on perm storage*/
-
-typedef enum {
-    HEALTH_REP      = 1,
-    EX_HEALTH_REP   = 2,
-    EVENTS_REP      = 3,
-    WOD_REP         = 4,
-    EXT_WOD_REP     = 5,
-    SU_SCI_HDR_REP  = 6,
-    ADCS_TLE_REP    = 7,
-    EPS_FLS_REP     = 8,
-    ECSS_STATS_REP  = 9,
-    LAST_STRUCT_ID  = 10
-}HK_struct_id;
-
-typedef enum {
-    P_OFF       = 0,
-    P_ON        = 1,
-    P_RESET     = 2,
-    SET_VAL     = 3,
-    LAST_FUN_ID = 4
-}FM_fun_id;
-
-typedef enum {
-    OBC_DEV_ID      = 1,
-    EPS_DEV_ID      = 2,
-    ADCS_DEV_ID     = 3,
-    COMMS_DEV_ID    = 4,
-    IAC_DEV_ID      = 5,
-    SU_DEV_ID       = 6,
-    GPS_DEV_ID      = 7,
-    OBC_SD_DEV_ID   = 8,
-    ADCS_SD_DEV_ID  = 9,
-    ADCS_SENSORS    = 10,
-    ADCS_GPS        = 11,
-    ADCS_MAGNETO    = 12,
-    ADCS_SPIN       = 13,
-    ADCS_TLE        = 14,
-    ADCS_CTRL_GAIN  = 15,
-    ADCS_SET_POINT  = 16,
-    EPS_WRITE_FLS   = 17,
-    SYS_DBG         = 18,
-    COMMS_WOD_PAT   = 19,
-    LAST_DEV_ID     = 20
-}FM_dev_id;
-
-/*Mass storage ids*/
-typedef enum {  
-    SU_NOSCRIPT     = 0, /*used when no script has ever been chosen to run, saved on SRAM*/
-    SU_SCRIPT_1     = 1,
-    SU_SCRIPT_2     = 2,
-    SU_SCRIPT_3     = 3,
-    SU_SCRIPT_4     = 4,
-    SU_SCRIPT_5     = 5,
-    SU_SCRIPT_6     = 6,
-    SU_SCRIPT_7     = 7,
-    SU_LOG          = 8,
-    WOD_LOG         = 9,
-    EXT_WOD_LOG     = 10,
-    EVENT_LOG       = 11,
-    FOTOS           = 12,
-    SCHS            = 13,
-    SRAM            = 14,
-    LAST_SID        = 15
-}MS_sid;
-
-typedef enum {
-    ALL         = 0,
-    TO          = 1,
-    BETWEEN     = 2,
-    SPECIFIC    = 3,
-    LAST_PART   = 4,
-    NO_MODE     = 5,
-    HARD_DELETE = 6,
-    DELETE_ALL  = 7,
-    FATFS_RESET = 8,
-    LAST_MODE   = 9
-}MS_mode;
-
-typedef enum {
-    SU_POWERED_OFF = 1,
-    SU_POWERED_ON  = 2,
-    SU_IDLE        = 3,
-    SU_FINISHED    = 4,
-    LAST_SU_STATE  = 5
-}SU_state;
-
-typedef enum {
-    EV_inc_pkt           = 1,
-    EV_pkt_ack_er        = 2,
-    EV_sys_boot          = 3,
-    EV_pwr_level         = 4,
-    EV_comms_tx_off      = 5,
-    EV_sys_timeout       = 6,
-    EV_sys_shutdown      = 7,
-    EV_assertion         = 8,
-    EV_su_error          = 9,
-    EV_su_scr_start      = 10,
-    EV_pkt_pool_timeout  = 11,
-    EV_ms_err            = 12,
-    LAST_EV_EVENT        = 13
-}EV_event;
-
-#define C_ASSERT(e)    ((e) ? (true) : (tst_debugging(__FILE_ID__, __LINE__, #e)))
-
-union _cnv {
-    double cnvD;
-    float cnvF;
-    uint32_t cnv32;
-    uint16_t cnv16[4];
-    uint8_t cnv8[8];
-};
-
-extern void HAL_uart_tx(TC_TM_app_id app_id, uint8_t *buf, uint16_t size);
-extern SAT_returnState event_log(uint8_t *buf, const uint16_t size);
-extern SAT_returnState event_crt_pkt_api(uint8_t *buf, uint8_t *f, uint16_t fi, uint32_t l, uint8_t *e, uint16_t *size, SAT_returnState mode);
-
-typedef struct {
-    /* packet id */
-    //uint8_t ver; /* 3 bits, should be equal to 0 */
-
-    //uint8_t data_field_hdr; /* 1 bit, data_field_hdr exists in data = 1 */
-    TC_TM_app_id app_id; /* TM: app id = 0 for time packets, = 0xff for idle packets. should be 11 bits only 8 are used though */
-    uint8_t type; /* 1 bit, tm = 0, tc = 1 */
-
-    /* packet sequence control */
-    uint8_t seq_flags; /* 3 bits, definition in TC_SEQ_xPACKET */
-    uint16_t seq_count; /* 14 bits, packet counter, should be unique for each app id */
-
-    uint16_t len; /* 16 bits, C = (Number of octets in packet data field) - 1, on struct is the size of data without the headers. on array is with the headers */
-
-    uint8_t ack; /* 4 bits, definition in TC_ACK_xxxx 0 if its a TM */
-    uint8_t ser_type; /* 8 bit, service type */
-    uint8_t ser_subtype; /* 8 bit, service subtype */
-
-    /*optional*/
-    //uint8_t pckt_sub_cnt; /* 8 bits*/
-    TC_TM_app_id dest_id;   /*on TC is the source id, on TM its the destination id*/
-
-    uint8_t *data; /* pkt data */
-
-    /*this is not part of the header. it is used from the software and the verification service,
-     *when the packet wants ACK.
-     *the type is SAT_returnState and it either stores R_OK or has the error code (failure reason).
-     *it is initialized as R_ERROR and the service should be responsible to make it R_OK or put the corresponding error.
-     */
-    SAT_returnState verification_state;
-/*  uint8_t padding;  x bits, padding for word alligment */
-
-//  uint16_t crc; /* CRC or checksum, mission specific*/
-}tc_tm_pkt;
-
-/*Lookup table that returns if a service with its subtype with TC or TM is supported and valid*/
-extern const uint8_t services_verification_TC_TM[MAX_SERVICES][MAX_SUBTYPES][2];
-extern uint8_t assertion_last_file;
-extern uint16_t assertion_last_line;
-
-uint8_t tst_debugging(uint16_t fi, uint32_t l, char *e);
 
 #endif
