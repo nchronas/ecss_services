3a8770a67cd86cc3878a3ca5376df2ed2882067a
nchronas <nchronas@gmail.com>
2018-01-17 10:48:28 +0100

    Removed application specific details.

--- core/pkt_pool.h
+++ core/pkt_pool.h
@@ -5,19 +5,6 @@
 #include "services.h"
 #include "system.h"
 
-#define PKT_TIMEOUT 60000 /*in mseconds*/
-#define PKT_NORMAL  198   /*MAX_PKT_DATA*/
-
-#if (SYSTEM_APP_ID == _EPS_APP_ID_)
-#define POOL_PKT_SIZE        10
-#define POOL_PKT_EXT_SIZE     0
-#define POOL_PKT_TOTAL_SIZE  10
-#else
-#define POOL_PKT_SIZE        16
-#define POOL_PKT_EXT_SIZE     4
-#define POOL_PKT_TOTAL_SIZE  20
-#endif
-
 tc_tm_pkt * get_pkt(uint16_t size);
 
 SAT_returnState free_pkt(tc_tm_pkt *pkt);
