858f491239fa6e22806508228b9e1cd44939ed81
nchronas <nchronas@gmail.com>
2018-03-14 14:36:22 +0100

    Adding seperate end flag

--- core/hldlc.c
+++ core/hldlc.c
@@ -1,30 +1,50 @@
 #include "hldlc.h"
-#include "upsat.h"
 
+#include <stddef.h>
+#include "packet_engine.h"
 
 #undef __FILE_ID__
 #define __FILE_ID__ 36
 
-SAT_returnState HLDLC_deframe(uint8_t *buf_in, uint8_t *buf_out, uint16_t *size) {
+SAT_returnState HLDLC_deframe(uint8_t *buf_in,
+                              uint8_t *buf_out,
+                              const uint16_t size_in,
+                              uint16_t *size_out) {
 
     if(!C_ASSERT(buf_in != 0 && buf_out != 0) == true)    { return SATR_ERROR; }
-    if(!C_ASSERT(buf_in[0] == HLDLC_START_FLAG) == true)  { return SATR_ERROR; } /*the first char should be a start flag*/
-    if(!C_ASSERT(*size <= UART_BUF_SIZE) == true)         { return SATR_ERROR; } //hard limits, check
 
     uint16_t cnt = 0;
 
-    for(uint16_t i = 1; i < *size; i++) {
+  //  for(uint16_t i = 1; i < size_in; i++) {
+  //          if(buf_in[i] == HLDLC_START_FLAG) {
+  //              cnt = 0;
+
+   // }
+
+    for(uint16_t i = 0; i < size_in; i++) {
         if(buf_in[i] == HLDLC_START_FLAG) {
-            *size = cnt;
+            cnt = 0;
+        } else if(buf_in[i] == HLDLC_STOP_FLAG) {
+            *size_out = cnt;
             return SATR_EOT;
         } else if(buf_in[i] == HLDLC_CONTROL_FLAG) {
             i++;
-            if(!C_ASSERT(i < (*size) - 1) == true)       { return SATR_ERROR; }
-            if(buf_in[i] == 0x5E) { buf_out[cnt++] = 0x7E; }
-            else if(buf_in[i] == 0x5D) { buf_out[cnt++] = 0x7D; }
-            else { return SATR_ERROR; }
+
+            if(buf_in[i] == 0x5E) {
+              buf_out[cnt++] = 0x7E;
+            } else if(buf_in[i] == 0x5D) {
+              buf_out[cnt++] = 0x7D;
+            } else if(buf_in[i] == 0x5C) {
+              buf_out[cnt++] = 0x7C;
+            } else {
+              return SATR_ERROR;
+            }
         } else {
             buf_out[cnt++] = buf_in[i];
+        }
+
+        if(!C_ASSERT(cnt < MAX_HLDLC_PKT_SIZE - 1) == true) {
+          return SATR_ERROR;
         }
     }
     return SATR_ERROR;
@@ -46,16 +66,24 @@ SAT_returnState HLDLC_frame(uint8_t *buf_in, uint8_t *buf_out, uint16_t *size) {
             if(buf_in[i] == HLDLC_START_FLAG) {
                 buf_out[cnt++] = HLDLC_CONTROL_FLAG;
                 buf_out[cnt++] = 0x5E;
+            } else if(buf_in[i] == HLDLC_STOP_FLAG) {
+                buf_out[cnt++] = HLDLC_CONTROL_FLAG;
+                buf_out[cnt++] = 0x5C;
             } else if(buf_in[i] == HLDLC_CONTROL_FLAG) {
                 buf_out[cnt++] = HLDLC_CONTROL_FLAG;
                 buf_out[cnt++] = 0x5D;
-            } else { buf_out[cnt++] = buf_in[i]; }
-            buf_out[cnt++] = HLDLC_START_FLAG;
+            } else {
+                buf_out[cnt++] = buf_in[i];
+            }
+            buf_out[cnt++] = HLDLC_STOP_FLAG;
             *size = cnt;
             return SATR_EOT;
         } else if(buf_in[i] == HLDLC_START_FLAG) {
             buf_out[cnt++] = HLDLC_CONTROL_FLAG;
             buf_out[cnt++] = 0x5E;
+        } else if(buf_in[i] == HLDLC_STOP_FLAG) {
+            buf_out[cnt++] = HLDLC_CONTROL_FLAG;
+            buf_out[cnt++] = 0x5C;
         } else if(buf_in[i] == HLDLC_CONTROL_FLAG) {
             buf_out[cnt++] = HLDLC_CONTROL_FLAG;
             buf_out[cnt++] = 0x5D;
--- core/hldlc.h
+++ core/hldlc.h
@@ -2,12 +2,16 @@
 #define __HLDLC_H
 
 #include <stdint.h>
-#include "services.h"
+#include "satellite.h"
 
 #define HLDLC_START_FLAG        0x7E
 #define HLDLC_CONTROL_FLAG      0x7D
+#define HLDLC_STOP_FLAG         0x7C
 
-SAT_returnState HLDLC_deframe(uint8_t *buf_in, uint8_t *buf_out, uint16_t *size);
+SAT_returnState HLDLC_deframe(uint8_t *buf_in,
+                              uint8_t *buf_out,
+                              uint16_t size_in,
+                              uint16_t *size_out);
 
 SAT_returnState HLDLC_frame(uint8_t *buf_in, uint8_t *buf_out, uint16_t *size);
 
