df2c5ba718cb41f6c8463dea57598e479a7e69fa
nchronas <nchronas@gmail.com>
2018-05-10 02:00:46 +0200

    Removed averaging

--- delfiPQ/EPS/devices.h
+++ delfiPQ/EPS/devices.h
@@ -4,22 +4,6 @@
 #include "satellite.h"
 #include "stdint.h"
 
-struct ina_array {
-  bool enabled;
-  int16_t current[5];
-
-  uint8_t number_of_data;
-  uint8_t data_counter;
-};
-
-struct ina_average {
-  bool enabled;
-  int16_t av[5];
-
-  uint8_t number_of_averages;
-  uint8_t average_counter;
-};
-
 struct ina_device {
   dev_id id;
   float current;
